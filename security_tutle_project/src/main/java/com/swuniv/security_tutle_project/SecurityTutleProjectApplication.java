744d6b60e65fcfe5a45ffa7a2e3f363038a186bf
ahngilung <lg2000f@naver.com>
2022-06-22 20:08:54 +0900

    1. Spring boot 프로젝트 생성 및 pom.xml 라이브러리 추가

---
+++ security_tutle_project/src/main/java/com/swuniv/security_tutle_project/SecurityTutleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.swuniv.security_tutle_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityTutleProjectApplication.class, args);
+    }
+
+}
---
+++ security_tutle_project/src/test/java/com/swuniv/security_tutle_project/SecurityTutleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swuniv.security_tutle_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityTutleProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
